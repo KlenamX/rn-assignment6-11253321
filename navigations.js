90d657fc6ad8f39f8714bf97adcd78640fe9cada
Klenam <cedeegbe@gmail.com>
2024-06-28 18:28:18 +0000

    Environment-setup

---
+++ navigations.js
@@ -0,0 +1,24 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator();
+
+import { View, Text } from "react-native";
+import React from "react";
+import HomeScreen from "./screens/HomeScreen";
+import CheckoutScreen from "./screens/CheckoutScreen";
+
+export default function Navigations() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Checkout" component={CheckoutScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
